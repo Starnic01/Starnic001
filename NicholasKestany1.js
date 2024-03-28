956787011e831b61dad1e20438e0b7025a9ff0b5
Starnic01 <160358427+Starnic01@users.noreply.github.com>
2024-03-28 06:51:53 +0300

    Add files via upload

---
+++ NicholasKestany1.js
@@ -0,0 +1,16 @@
+// Function to add two numbers
+function addNumbers(num1, num2) {
+    // Add num1 and num2
+    let sum = num1 + num2;
+    
+    // Return the result
+    return sum;
+}
+
+
+// Test cases
+console.log(addNumbers(3, 5)); // Expected output: 8
+console.log(addNumbers(-1, 7)); // Expected output: 6
+console.log(addNumbers(0, 0)); // Expected output: 0
+
+
